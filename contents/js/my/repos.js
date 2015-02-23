c2fa6e7d16e27d45ca2345f6e3f1bd23415eeeb3
Ryan Milvenan <ryanmilvenan@gmail.com>
2015-02-22 20:13:18 -0700

    build

--- contents/js/my/repos.js
+++ contents/js/my/repos.js
@@ -3,5 +3,22 @@ function myRepos(){
     // display my repos in #list
     // e.g., https://api.github.com/users/doubleshow/repos
 
-    alert('todo')
+    $.get("https://api.github.com/users/doubleshow/repos", github, function(data) {
+
+        var repos = data
+
+        $.get("/git-jquery/templates/myRepoList.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: repos})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            // load the first repo to view
+            myRepo(repos[0].full_name)
+
+        })
+
+    })
 }
