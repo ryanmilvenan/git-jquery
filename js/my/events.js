bd4acf3db612c6c7e5f103abbedcc69e646b79e8
Ryan Milvenan <ryanmilvenan@gmail.com>
2015-02-22 20:13:18 -0700

    build

--- js/my/events.js
+++ js/my/events.js
@@ -2,6 +2,18 @@ function myEvents(){
     
     // display a list of events belonging to me in #list
     // e.g., https://api.github.com/users/doubleshow/events
+    $.get("https://api.github.com/users/doubleshow/events", github, function(data) {
 
-    alert('todo')
+        var events = data
+
+        $.get("/git-jquery/templates/myEventList.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: events})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+        })
+    })
 }
